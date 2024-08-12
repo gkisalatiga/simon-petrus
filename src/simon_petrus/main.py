32622c8334efdedf3a63d6289d11773129761062
groaking <93555329+groaking@users.noreply.github.com>
2024-08-13 00:08:15 +0700

    Added "transfer bank method" menu

--- src/simon_petrus/main.py
+++ src/simon_petrus/main.py
@@ -31,9 +31,11 @@ REFERENCES:
     - https://github.com/shailshouryya/save-thread-result
     [10] Prevent window resizing in PyQt5
     - https://stackoverflow.com/a/13775478
+    [11] Capitalize the first letter of words in a sentence
+    - https://stackoverflow.com/a/1549644
 """
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 from PyQt5.QtCore import pyqtSlot, QPoint
 import base64
 import json
@@ -52,9 +54,11 @@ from lib.string_validator import StringValidator
 from lib.uploader import Uploader
 from loading_animation import ScreenLoadingAnimation
 from ui import dialog_forms
+from ui import dialog_persembahan
 from ui import frame_default
 from ui import frame_formulir
 from ui import frame_liturgi_upload
+from ui import frame_persembahan
 from ui import frame_renungan
 from ui import frame_social_media
 from ui import frame_warta_upload
@@ -389,6 +393,7 @@ class ScreenMain(QtWidgets.QMainWindow, screen_main.Ui_MainWindow):
         const_fragment_dictionary = {
             'fragment_default': FrameDefault(),
             'fragment_formulir': FrameFormulir(),
+            'fragment_persembahan': FramePersembahan(),
             'fragment_renungan': FrameRenungan(),
             'fragment_social_media': FrameSocialMedia(),
             'fragment_tata_ibadah': FrameTataIbadah(),
@@ -537,6 +542,16 @@ class ScreenMain(QtWidgets.QMainWindow, screen_main.Ui_MainWindow):
         pass
 
     @pyqtSlot()
+    def on_cmd_persembahan_clicked(self):
+        global cur_fragment
+        cur_fragment = 'fragment_persembahan'
+        self.clear_fragment_and_display(cur_fragment)
+
+        # Prevents freezing [5]
+        QtCore.QCoreApplication.processEvents()
+        pass
+
+    @pyqtSlot()
     def on_cmd_renungan_clicked(self):
         global cur_fragment
         cur_fragment = 'fragment_renungan'
@@ -598,6 +613,34 @@ class DialogForms(QtWidgets.QDialog, dialog_forms.Ui_Dialog):
             self.findChild(QtWidgets.QDialogButtonBox, 'button_box').buttons()[0].setEnabled(False)
         elif not url.startswith('https://'):
             self.findChild(QtWidgets.QLabel, 'label_status').setText('URL Anda harus dimulai dengan "https://"!')
+            self.findChild(QtWidgets.QDialogButtonBox, 'button_box').buttons()[0].setEnabled(False)
+        else:
+            self.findChild(QtWidgets.QLabel, 'label_status').setText('-')
+            self.findChild(QtWidgets.QDialogButtonBox, 'button_box').buttons()[0].setEnabled(True)
+
+
+class DialogPersembahan(QtWidgets.QDialog, dialog_persembahan.Ui_Dialog):
+    def __init__(self, *args, obj=None, title='', **kwargs):
+        super(DialogPersembahan, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        # Prevent resizing. [10]
+        self.setFixedSize(self.size())
+
+        # Input fields validation.
+        self.field_bank_abbr.textChanged.connect(self.validate_fields)
+        self.field_bank_name.textChanged.connect(self.validate_fields)
+        self.field_number.textChanged.connect(self.validate_fields)
+        self.field_holder.textChanged.connect(self.validate_fields)
+
+    def validate_fields(self):
+        bank_abbr = self.findChild(QtWidgets.QLineEdit, 'field_bank_abbr').text().strip()
+        bank_name = self.findChild(QtWidgets.QLineEdit, 'field_bank_name').text().strip()
+        number = self.findChild(QtWidgets.QLineEdit, 'field_number').text().strip()
+        holder = self.findChild(QtWidgets.QLineEdit, 'field_holder').text().strip()
+
+        if bank_abbr == '' or bank_name == '' or number == '' or holder == '':
+            self.findChild(QtWidgets.QLabel, 'label_status').setText('Anda harus memasukkan semua masukan!')
             self.findChild(QtWidgets.QDialogButtonBox, 'button_box').buttons()[0].setEnabled(False)
         else:
             self.findChild(QtWidgets.QLabel, 'label_status').setText('-')
@@ -795,14 +838,6 @@ class FrameFormulir(QtWidgets.QFrame, frame_formulir.Ui_Frame):
         title = self.d.findChild(QtWidgets.QLineEdit, 'field_title').text().strip()
         url = self.d.findChild(QtWidgets.QLineEdit, 'field_url').text().strip()
 
-        # Prevents empty form field.
-        if title == '' or url == '':
-            QtWidgets.QMessageBox.warning(
-                self, 'Data kosong!',
-                'Anda harus mengisi ',
-                QtWidgets.QMessageBox.Ok
-            )
-
         if self.action == 'new':
             Lg('main.FrameFormulir.on_dialog_forms_accepted', f'Creating a new form: {title} ...')
 
@@ -850,6 +885,299 @@ class FrameFormulir(QtWidgets.QFrame, frame_formulir.Ui_Frame):
             # Prefill with existing values.
             self.d.findChild(QtWidgets.QLineEdit, 'field_title').setText(edit_title)
             self.d.findChild(QtWidgets.QLineEdit, 'field_url').setText(edit_url)
+
+        # Show the dialog.
+        self.d.show()
+
+        # Validate preliminary field values.
+        self.d.validate_fields()
+
+
+class FramePersembahan(QtWidgets.QFrame, frame_persembahan.Ui_Frame):
+
+    # The QListWidgetItem default role.
+    DEFAULT_ITEM_ROLE = 1048577  # --- 2**20 + 1
+
+    def __init__(self, *args, obj=None, **kwargs):
+        super(FramePersembahan, self).__init__(*args, **kwargs)
+        self.action = None
+        self.cur_item = None
+        self.setupUi(self)
+
+        # Initiating the prompt dialog.
+        self.d = DialogPersembahan(self)
+
+        # Populating the forms list with existing forms.
+        self.init_prefilled_banks()
+
+        # Add slot connector.
+        self.d.accepted.connect(self.on_dialog_banks_accepted)
+        self.list_banks.currentItemChanged.connect(self.on_current_item_changed)
+
+    def init_prefilled_banks(self):
+        """
+        Populate the QListWidget with the bank list found in the GKI Salatiga+ JSON data.
+        :return: nothing.
+        """
+        # Iterating through every list of existing forms in the JSON schema.
+        for a in app_db.db['offertory']:
+            # Adding the list item.
+            b = QtWidgets.QListWidgetItem()
+            b.setData(
+                self.DEFAULT_ITEM_ROLE,
+                (a['bank-name'], a['bank-abbr'], a['bank-number'], a['account-holder'])
+            )
+            b.setText(f'{a['bank-abbr']} {a['bank-number']}')
+            self.findChild(QtWidgets.QListWidget, 'list_banks').addItem(b)
+
+    def on_current_item_changed(self):
+        # Save the state of the currently selected item.
+        self.cur_item = self.findChild(QtWidgets.QListWidget, 'list_banks').currentItem()
+
+        # The item widget's size.
+        widget_size = self.findChild(QtWidgets.QListWidget, 'list_banks').__len__()
+
+        # The selected item's index.
+        a = self.findChild(QtWidgets.QListWidget, 'list_banks').currentItem()
+        y_pos = self.findChild(QtWidgets.QListWidget, 'list_banks').indexFromItem(a).row()
+
+        if y_pos == 0:
+            self.findChild(QtWidgets.QPushButton, 'btn_move_down').setEnabled(True)
+            self.findChild(QtWidgets.QPushButton, 'btn_move_up').setEnabled(False)
+        elif y_pos == widget_size - 1:
+            self.findChild(QtWidgets.QPushButton, 'btn_move_down').setEnabled(False)
+            self.findChild(QtWidgets.QPushButton, 'btn_move_up').setEnabled(True)
+        else:
+            self.findChild(QtWidgets.QPushButton, 'btn_move_down').setEnabled(True)
+            self.findChild(QtWidgets.QPushButton, 'btn_move_up').setEnabled(True)
+
+        # Update the display data.
+        item_data = self.findChild(QtWidgets.QListWidget, 'list_banks').item(y_pos).data(self.DEFAULT_ITEM_ROLE)
+        self.findChild(QtWidgets.QLabel, 'label_bank_name').setText(item_data[0])
+        self.findChild(QtWidgets.QLabel, 'label_bank_abbr').setText(item_data[1])
+        self.findChild(QtWidgets.QLabel, 'label_number').setText(item_data[2])
+        self.findChild(QtWidgets.QLabel, 'label_holder').setText(item_data[3])
+
+    @pyqtSlot()
+    def on_btn_add_clicked(self):
+        # Prompt for user input value.
+        self.call_action('new')
+
+    @pyqtSlot()
+    def on_btn_delete_clicked(self):
+        if self.cur_item is None:
+            return
+
+        # The title of the currently selected item.
+        title = self.cur_item.text()
+
+        # Warn the user about deletion.
+        r = (QtWidgets.QMessageBox.warning(
+            self, 'Penghapusan data transfer bank.',
+            f'Apakah Anda yakin akan menghapus transfer bank: {title} dari GKI Salatiga+?'
+            f'\nTindakan ini tidak dapat dikembalikan!',
+            QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No
+        ))
+
+        # Validating the response.
+        if r == QtWidgets.QMessageBox.Yes:
+            # Get the selected item's row position.
+            y_pos = self.findChild(QtWidgets.QListWidget, 'list_banks').indexFromItem(self.cur_item).row()
+
+            # Remove the selected item from the list.
+            self.findChild(QtWidgets.QListWidget, 'list_banks').takeItem(y_pos)
+
+            # Logging.
+            Lg('main.FramePersembahan.on_btn_delete_clicked', f'Removed the bank info: {title} successfully!')
+        else:
+            Lg('main.FramePersembahan.on_btn_delete_clicked', f'Phew! It did not get removed.')
+
+    @pyqtSlot()
+    def on_btn_edit_clicked(self):
+        if self.cur_item is None:
+            return
+
+        # The selected item's data.
+        item_data = self.cur_item.data(self.DEFAULT_ITEM_ROLE)
+        bank_name = item_data[0]
+        bank_abbr = item_data[1]
+        number = item_data[2].replace('.', '')
+        holder = item_data[3]
+
+        # Prompt for user input value.
+        self.call_action('edit', bank_abbr, bank_name, number, holder)
+
+    @pyqtSlot()
+    def on_btn_move_down_clicked(self):
+        if self.cur_item is None:
+            return
+
+        # Clone the selected item.
+        a = self.cur_item.clone()
+
+        # The item widget's size.
+        widget_size = self.findChild(QtWidgets.QListWidget, 'list_banks').__len__()
+
+        # Get the selected item's row position.
+        y_pos = self.findChild(QtWidgets.QListWidget, 'list_banks').indexFromItem(self.cur_item).row()
+
+        # Do not move up if already at the top.
+        if y_pos == widget_size - 1:
+            return
+
+        # Remove the selected item from the list.
+        self.findChild(QtWidgets.QListWidget, 'list_banks').takeItem(y_pos)
+
+        # Move up the item.
+        target_pos = y_pos + 1
+        self.findChild(QtWidgets.QListWidget, 'list_banks').insertItem(target_pos, a)
+        self.findChild(QtWidgets.QListWidget, 'list_banks').setCurrentRow(target_pos)
+
+    @pyqtSlot()
+    def on_btn_move_up_clicked(self):
+        if self.cur_item is None:
+            return
+
+        # Clone the selected item.
+        a = self.cur_item.clone()
+
+        # Get the selected item's row position.
+        y_pos = self.findChild(QtWidgets.QListWidget, 'list_banks').indexFromItem(self.cur_item).row()
+
+        # Do not move up if already at the top.
+        if y_pos == 0:
+            return
+
+        # Remove the selected item from the list.
+        self.findChild(QtWidgets.QListWidget, 'list_banks').takeItem(y_pos)
+
+        # Move up the item.
+        target_pos = y_pos - 1
+        self.findChild(QtWidgets.QListWidget, 'list_banks').insertItem(target_pos, a)
+        self.findChild(QtWidgets.QListWidget, 'list_banks').setCurrentRow(target_pos)
+
+    @pyqtSlot()
+    def on_btn_save_clicked(self):
+        # Creating the JSON array to replace the old one.
+        a = []
+
+        # Iterating through every item.
+        for i in range(self.findChild(QtWidgets.QListWidget, 'list_banks').__len__()):
+
+            # The list item.
+            b = self.findChild(QtWidgets.QListWidget, 'list_banks').item(i)
+
+            # The item title and URL.
+            item_data = b.data(self.DEFAULT_ITEM_ROLE)
+            bank_name = item_data[0]
+            bank_abbr = item_data[1]
+            number = item_data[2]
+            holder = item_data[3]
+
+            # Add this item to the JSON array.
+            a.append({
+                'bank-name': bank_name,
+                'bank-abbr': bank_abbr,
+                'bank-number': number,
+                'account-holder': holder
+            })
+
+        # Overwrite the existing forms object.
+        app_db.db['offertory'] = a
+
+        # Save to local file.
+        app_db.save_local('offertory')
+
+        # Display the save successful notice.
+        QtWidgets.QMessageBox.information(
+            self, 'Data tersimpan!',
+            'Perubahan data berhasil disimpan. Silahkan unggah pembaruan supaya data dapat dilihat oleh jemaat.',
+            QtWidgets.QMessageBox.Ok
+        )
+
+    def on_dialog_banks_accepted(self):
+        # The input dialog's field data.
+        bank_name = self.d.findChild(QtWidgets.QLineEdit, 'field_bank_name').text().strip()
+        bank_abbr = self.d.findChild(QtWidgets.QLineEdit, 'field_bank_abbr').text().strip()
+        number = self.d.findChild(QtWidgets.QLineEdit, 'field_number').text().strip()
+        holder = self.d.findChild(QtWidgets.QLineEdit, 'field_holder').text().strip()
+
+        # Visually and string-wise edit the field data to match standard. [11]
+        bank_abbr = bank_abbr.upper()
+
+        s = [w for w in number]
+        s.insert(3, '.') if len(s) >= 3 else s
+        s.insert(6, '.') if len(s) >= 6 else s
+        s.insert(10, '.') if len(s) >= 10 else s
+        number = ''.join(s)
+
+        # The display title.
+        display_text = f'{bank_abbr} {number}'
+
+        if self.action == 'new':
+            Lg('main.FramePersembahan.on_dialog_banks_accepted', f'Creating a new bank: {display_text} ...')
+
+            # Add a new item to the list.
+            # Adding the list item.
+            a = QtWidgets.QListWidgetItem()
+            a.setData(
+                self.DEFAULT_ITEM_ROLE,
+                (bank_name, bank_abbr, number, holder)
+            )
+            a.setText(display_text)
+            self.findChild(QtWidgets.QListWidget, 'list_banks').addItem(a)
+
+            # Set the focus to the newly created item.
+            self.findChild(QtWidgets.QListWidget, 'list_banks').setCurrentItem(a)
+
+        elif self.action == 'edit':
+            Lg('main.FramePersembahan.on_dialog_forms_accepted', f'Editing an existing bank info: {display_text} ...')
+
+            # Edit the selected item's value.
+            self.cur_item.setText(display_text)
+            self.cur_item.setData(
+                self.DEFAULT_ITEM_ROLE,
+                (bank_name, bank_abbr, number, holder)
+            )
+
+        # Update the current selection and state.
+        self.on_current_item_changed()
+
+    def call_action(self, action,
+                    edit_bank_abbr: str = '',
+                    edit_bank_name: str = '',
+                    edit_number: str = '',
+                    edit_holder: str = ''):
+        """
+        Determine what bank action to take, as well as displaying the dialog.
+        Possible values; 'new' and 'edit'.
+        :param action: between 'new' and 'edit', specifies the bank action to undergo.
+        :param edit_bank_abbr: (optional) the current bank's abbreviation to edit.
+        :param edit_bank_name: (optional) the current bank's full name to edit.
+        :param edit_number: (optional) the current bank's account number to edit.
+        :param edit_holder: (optional) the current bank's account holder to edit.
+        :return: nothing.
+        """
+        self.action = action
+
+        # Change the dialog's title according to the passed value.
+        if action == 'new':
+            self.d.findChild(QtWidgets.QLabel, 'app_title').setText('Tambahkan Info Transfer Bank Baru')
+
+            # Clear the existing title and URL.
+            self.d.findChild(QtWidgets.QLineEdit, 'field_bank_abbr').setText('')
+            self.d.findChild(QtWidgets.QLineEdit, 'field_bank_name').setText('')
+            self.d.findChild(QtWidgets.QLineEdit, 'field_number').setText('')
+            self.d.findChild(QtWidgets.QLineEdit, 'field_holder').setText('')
+
+        elif action == 'edit':
+            self.d.findChild(QtWidgets.QLabel, 'app_title').setText('Edit Info Transfer Bank')
+
+            # Prefill with existing values.
+            self.d.findChild(QtWidgets.QLineEdit, 'field_bank_abbr').setText(edit_bank_abbr)
+            self.d.findChild(QtWidgets.QLineEdit, 'field_bank_name').setText(edit_bank_name)
+            self.d.findChild(QtWidgets.QLineEdit, 'field_number').setText(edit_number)
+            self.d.findChild(QtWidgets.QLineEdit, 'field_holder').setText(edit_holder)
 
         # Show the dialog.
         self.d.show()
